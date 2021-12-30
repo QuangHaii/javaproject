d888707d732fc5e6941429c0f88c09ac1b683861
QuangHaii <hainoob2002@gmail.com>
2021-12-30 12:50:45 +0700

    update

--- src/Product/Drink.java
+++ src/Product/Drink.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class Drink extends Product {
 	private String drinkType;
-	private int inStock;
 	
 	public Drink() {
 		super();
 		productType="Drink";
 	}
 	
-	public Drink(String drinkType, int inStock) {
+	public Drink(String drinkType) {
 		super();
 		this.drinkType = drinkType;
-		this.inStock = inStock;
 	}
 
 	public String getdrinkType() {
@@ -25,14 +23,6 @@ public class Drink extends Product {
 		this.drinkType = drinkType;
 	}
 
-	public int getInStock() {
-		return inStock;
-	}
-
-	public void setInStock(int inStock) {
-		this.inStock = inStock;
-	}
-
 	@SuppressWarnings("resource")
 	@Override
 	public void input() {
@@ -40,13 +30,11 @@ public class Drink extends Product {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Nhập loại đồ uống: ");
 		drinkType=scanner.nextLine();
-		System.out.println("Nhập số lượng tồn kho: ");
-		inStock=Integer.parseInt(scanner.nextLine());
 	}
 	
 	@Override
 	public String toString() {
-		return super.toString()+"|Loại đồ uống: "+drinkType+"|Tồn kho: "+inStock;
+		return super.toString()+"|Loại đồ uống: "+drinkType;
 	}
 	
 	public void output() {
